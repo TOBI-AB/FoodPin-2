34dfcef5d0d2a04bcc4228a8c13ace6cd9aa9461
leversol <test_win8@outlook.com>
2015-12-01 10:27:23 +0000

    Initial Commit

---
+++ FoodPin/Restaurant.swift
@@ -0,0 +1,23 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 20/8/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Restaurant: NSManagedObject {
+    @NSManaged var name: String
+    @NSManaged var type: String
+    @NSManaged var location: String
+    @NSManaged var phoneNumber: String?
+    @NSManaged var image: NSData?
+    @NSManaged var isVisited: NSNumber?
+    @NSManaged var rating: String?
+    
+    
+}
+
